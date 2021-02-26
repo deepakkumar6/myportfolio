1bc6f9cba05b182bfceb2e3169562ac82883e4f8
Deepak Kumar <deepakkumar006007@gmail.com>
2021-02-26 19:23:05 +0530

    Projects Added

--- script.js
+++ script.js
@@ -21,14 +21,14 @@ $(document).ready(function(){
 
     // typing animation script
     var typed = new Typed(".typing",{
-        strings:['problem solver','bloger','freelancer','competitive programmer'],
+        strings:['Problem Solver','Web Developer','Python Developer','Competitive Programmer','Software Developer'],
         typeSpeed:100,
         backSpeed:60,
         loop:true
 
     });
     var typed = new Typed(".typing-2",{
-        strings:['problem solver','bloger','freelancer','competitive programmer'],
+        strings:['Problem Solver','Web Developer','Python Developer','Competitive Programmer','Software Developer'],
         typeSpeed:100,
         backSpeed:60,
         loop:true
